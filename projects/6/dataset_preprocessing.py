86f5a684040d989b41ab275f512d1dca4a11d946
Kravchenko01 <roma.kravchenco.2001@mail.ru>
2024-05-04 16:50:59 +0000

    fix hw6

--- projects/6/dataset_preprocessing.py
+++ projects/6/dataset_preprocessing.py
@@ -1,10 +1,5 @@
 import argparse
 
-from pyspark.ml import Pipeline
-from pyspark.ml.feature import *
-
-from pyspark.sql.types import *
-import pyspark.sql.functions as f
 from pyspark.sql import SparkSession
 
 
@@ -19,27 +14,7 @@ spark = SparkSession.builder.getOrCreate()
 spark.sparkContext.setLogLevel('WARN')
 
 
-tokenizer = Tokenizer(inputCol="reviewText", outputCol="reviewText_words")
-
-stop_words = StopWordsRemover.loadDefaultStopWords("english")
-swr = StopWordsRemover(inputCol=tokenizer.getOutputCol(), outputCol="reviewText_words_filtered", stopWords=stop_words)
-
-count_vectorizer = CountVectorizer(inputCol=swr.getOutputCol(), outputCol="word_vector")
-
-assembler = VectorAssembler(inputCols=[
-    count_vectorizer.getOutputCol(),
-],outputCol="features")
-
-pipeline = Pipeline(stages=[
-    tokenizer,
-    swr,
-    count_vectorizer,
-    assembler,
-])
-
 train_data = spark.read.json(args.path_in).fillna("null", subset=("reviewText"))
-
-#train_data = pipeline.fit(train_data).transform(train_data)
 
 if "label" in train_data.columns:
     train_data = train_data.select("label", "reviewText", "id")
